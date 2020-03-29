f373e9c34b36045e08386dc4e42cec68582e5bdd
a3event <62447367+a3event@users.noreply.github.com>
2020-03-29 14:04:09 -0400

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -14,6 +14,12 @@ $( document ).ready(function() {
 		changeTheme(localStorage.userTheme);
 	}
 	
+	showGacha = document.getElementById("show_gacha");
+	showFree = document.getElementById("show_free");
+	showSSR = document.getElementById("show_ssr");
+	showSR = document.getElementById("show_sr");
+	showFuture = document.getElementById("show_future");
+	
 });
 
 function filterSkills() {
@@ -50,22 +56,26 @@ function filterCards(id) {
 	var checkBox = document.getElementById(id);
 	var table = $('table.evt5 td');
 	
-	//Unique classes: hide_gacha, hide_free, hide_ssr, hide_sr
-	
 	if (id == "show_gacha") {
 		if (checkBox.checked == false){
+			if (showFree.checked == false){
+				showFree.checked = true;
+				filterCards("show_free");
+			}
 			for(var i=0; i<table.length; i++){
 				if(table[i].classList.contains('gacha_ssr') || table[i].classList.contains('gacha_sr')){
-					table.eq(i).css('visibility', 'hidden');
 					table[i].classList.add("hide_gacha");
+					table.eq(i).css('visibility', 'hidden');
 				}
 			}
 		}
-		else {
+		else if (checkBox.checked == true){
 			for(var i=0; i<table.length; i++){
 				if(table[i].classList.contains('gacha_ssr') || table[i].classList.contains('gacha_sr')){
-					table.eq(i).css('visibility', 'visible');
 					table[i].classList.remove("hide_gacha");
+					if(!(table[i].classList.contains('hide_future'))){
+						table.eq(i).css('visibility', 'visible');
+					}
 				}
 			}
 			filterCards("show_ssr");
@@ -75,18 +85,24 @@ function filterCards(id) {
 	
 	if (id == "show_free") {
 		if (checkBox.checked == false){
+			if (showGacha.checked == false){
+				showGacha.checked = true;
+				filterCards("show_gacha");
+			}
 			for(var i=0; i<table.length; i++){
 				if(table[i].classList.contains('free_ssr') || table[i].classList.contains('free_sr')){
-					table.eq(i).css('visibility', 'hidden');
 					table[i].classList.add("hide_free");
+					table.eq(i).css('visibility', 'hidden');
 				}
 			}
 		}
-		else {
+		else if (checkBox.checked == true){
 			for(var i=0; i<table.length; i++){
 				if(table[i].classList.contains('free_ssr') || table[i].classList.contains('free_sr')){
-					table.eq(i).css('visibility', 'visible');
 					table[i].classList.remove("hide_free");
+					if(!(table[i].classList.contains('hide_future'))){
+						table.eq(i).css('visibility', 'visible');
+					}
 				}
 			}
 			filterCards("show_ssr");
@@ -96,15 +112,19 @@ function filterCards(id) {
 	
 	if (id == "show_ssr") {
 		if (checkBox.checked == false){
+			if (showSR.checked == false){
+				showSR.checked = true;
+				filterCards("show_sr");
+			}
 			for(var i=0; i<table.length; i++){
 				if(table[i].classList.contains('gacha_ssr') || table[i].classList.contains('free_ssr')){
 					table.eq(i).css('visibility', 'hidden');
 				}
 			}
 		}
-		else {
+		else if (checkBox.checked == true){
 			for(var i=0; i<table.length; i++){
-				if((table[i].classList.contains('gacha_ssr') || table[i].classList.contains('free_ssr')) && !(table[i].classList.contains('hide_gacha')) && !(table[i].classList.contains('hide_free'))){
+				if((table[i].classList.contains('gacha_ssr') || table[i].classList.contains('free_ssr')) && !(table[i].classList.contains('hide_gacha')) && !(table[i].classList.contains('hide_free')) && !(table[i].classList.contains('hide_future'))){
 					table.eq(i).css('visibility', 'visible');
 				}
 			}
@@ -113,18 +133,45 @@ function filterCards(id) {
 	
 	if (id == "show_sr") {
 		if (checkBox.checked == false){
+			if (showSSR.checked == false){
+				showSSR.checked = true;
+				filterCards("show_ssr");
+			}
 			for(var i=0; i<table.length; i++){
 				if(table[i].classList.contains('gacha_sr') || table[i].classList.contains('free_sr')){
 					table.eq(i).css('visibility', 'hidden');
 				}
 			}
 		}
-		else {
+		else if (checkBox.checked == true){
 			for(var i=0; i<table.length; i++){
-				if((table[i].classList.contains('gacha_sr') || table[i].classList.contains('free_sr')) && !(table[i].classList.contains('hide_gacha')) && !(table[i].classList.contains('hide_free'))){
+				if((table[i].classList.contains('gacha_sr') || table[i].classList.contains('free_sr')) && !(table[i].classList.contains('hide_gacha')) && !(table[i].classList.contains('hide_free')) && !(table[i].classList.contains('hide_future'))){
 					table.eq(i).css('visibility', 'visible');
 				}
 			}
+		}
+	}
+	
+	if (id == "show_future") {
+		if (checkBox.checked == false){
+			for(var i=0; i<table.length; i++){
+				if(table[i].classList.contains('future')){
+					table.eq(i).css('visibility', 'hidden');
+					table[i].classList.add("hide_future");
+				}
+			}
+		}
+		else {
+			for(var i=0; i<table.length; i++){
+				if(table[i].classList.contains('future')){
+					table[i].classList.remove("hide_future");
+					if(!(table[i].classList.contains('hide_gacha')) && !(table[i].classList.contains('hide_free'))){
+						table.eq(i).css('visibility', 'visible');
+					}
+				}
+			}
+			filterCards("show_ssr");
+			filterCards("show_sr");
 		}
 	}
 }
@@ -132,10 +179,10 @@ function filterCards(id) {
 function changeTheme(t) {
 	
 	if (t == "theme1") {
-		document.getElementById('theme_css').href = 'https://a3event.github.io/css/theme1.css';
+		document.getElementById('theme_css').href = 'C:/Users/Ruby/Documents/T/css/theme1.css';
 	}
 	else {
-		document.getElementById('theme_css').href = 'https://a3event.github.io/css/theme2.css';
+		document.getElementById('theme_css').href = 'C:/Users/Ruby/Documents/T/css/theme2.css';
 	}
 	
 	localStorage.setItem("userTheme", t);
